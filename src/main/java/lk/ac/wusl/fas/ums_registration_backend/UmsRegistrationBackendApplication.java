be48b649e9e715b1eb49ca39ccb044cbf4321b1c
Kavindu Ranasinghe <109368178+KavinduRanasinghe@users.noreply.github.com>
2026-05-03 02:50:05 +0530

    init commit

---
+++ src/main/java/lk/ac/wusl/fas/ums_registration_backend/UmsRegistrationBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.wusl.fas.ums_registration_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsRegistrationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsRegistrationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/wusl/fas/ums_registration_backend/UmsRegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.wusl.fas.ums_registration_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsRegistrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
